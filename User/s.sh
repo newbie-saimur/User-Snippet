1167a3ac9bb7ef6a7cbfdfa6e4dcce2a102c647e
Md Saimur Rahman Robin <saimurrahmanarnob@gmail.com>
2024-04-17 23:39:36 +0600

    Add files via upload

--- User/s.sh
+++ User/s.sh
@@ -1,12 +1,19 @@
 for((i = 1; ; i++)); do
 	echo $i
 	./gen $i > input2.in
-	./main < inputf.in > outputf.in
+	./main < input2.in > outputf.in
 	./brute < input2.in > output2.in
 	diff -w outputf.in output2.in || break
 	# diff -w <(./main < input2.in) <(./brute < input2.in) || break
 done
 echo -e
+echo 'Testcase : '
 cat input2.in
+echo -e
+echo 'Wrong Output :'
+cat outputf.in
+echo -e
+echo 'Desired Output :' 
+cat output2.in
 echo -e
 echo -e
